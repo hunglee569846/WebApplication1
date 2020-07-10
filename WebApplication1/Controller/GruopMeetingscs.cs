c750f8e15a856eaeb6c24d14009f81bc6b9ebf0c
LE HUY HUNG <1621050181@student.humg.edu.vn>
2020-07-10 17:43:23 +0700

    Add project files.

---
+++ WebApplication1/Controller/GruopMeetingscs.cs
@@ -0,0 +1,108 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controller
+{
+    public class GruopMeetingscs
+    {
+        public static IEnumerable<GroupMeeting> GetGroupMeetings()
+        {
+            List<GroupMeeting> groupMeetingsList = new List<GroupMeeting>();
+            using (IDbConnection con = new SqlConnection(GroupMeeting.strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                groupMeetingsList = con.Query<GroupMeeting>("GetGroupMeetingDetails").ToList();
+            }
+
+            return groupMeetingsList;
+        }
+        public static GroupMeeting GetGroupMeetingByID(int? Id)
+        {
+            GroupMeeting groupMeeting = new GroupMeeting();
+            if (Id == null)
+                return groupMeeting;
+            using (IDbConnection con = new SqlConnection(GroupMeeting.strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed) con.Open();
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@Id", Id);
+                groupMeeting = con.Query<GroupMeeting>
+                ("GetGroupMeetingByID", parameters, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+            return groupMeeting;
+        }
+
+        public static int AddGroupMeeting(GroupMeeting groupMeeting)
+        {
+            int rowAffectted = 0;
+            try
+            {
+
+                using (IDbConnection con = new SqlConnection(GroupMeeting.strConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed) con.Open();
+                    DynamicParameters parameters = new DynamicParameters();
+                    parameters.Add("@ProjectName", groupMeeting.ProjectName);
+                    parameters.Add("@GroupMeetingLeadName", groupMeeting.GroupMeetingLeadName);
+                    parameters.Add("@TeamLeadName", groupMeeting.TeamLeadName);
+                    parameters.Add("@Description", groupMeeting.Description);
+                    parameters.Add("@GroupMeetingDate", groupMeeting.GroupMeetingDate);
+
+                    rowAffectted = con.Execute("InsertGroupMeeting", parameters, commandType: CommandType.StoredProcedure);
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            return rowAffectted;
+        }
+        public static int UpdateGroupMeeting(GroupMeeting groupMeeting)
+        {
+            {
+                int rowAffected = 0;
+
+                using (IDbConnection con = new SqlConnection(GroupMeeting.strConnectionString))
+                {
+                    if (con.State == ConnectionState.Closed)
+                        con.Open();
+
+                    DynamicParameters parameters = new DynamicParameters();
+                    parameters.Add("@Id", groupMeeting.Id);
+                    parameters.Add("@ProjectName", groupMeeting.ProjectName);
+                    parameters.Add("@GroupMeetingLeadName", groupMeeting.GroupMeetingLeadName);
+                    parameters.Add("@TeamLeadName", groupMeeting.TeamLeadName);
+                    parameters.Add("@Description", groupMeeting.Description);
+                    parameters.Add("@GroupMeetingDate", groupMeeting.GroupMeetingDate);
+                    rowAffected = con.Execute("UpdateGroupMeeting", parameters, commandType: CommandType.StoredProcedure);
+                }
+
+                return rowAffected;
+            }
+        }
+
+        public static int DeleteGroupMeeting(int id)
+        {
+            int rowAffected = 0;
+            using (IDbConnection con = new SqlConnection(GroupMeeting.strConnectionString))
+            {
+                if (con.State == ConnectionState.Closed)
+                    con.Open();
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("@Id", id);
+                rowAffected = con.Execute("DeleteGroupMeeting", parameters, commandType: CommandType.StoredProcedure);
+            }
+            return rowAffected;
+        }
+    }
+}
---
+++ WebApplication1/Models/GroupMeeting.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace WebApplication1.Models
+{
+    //mã tao thuộc tính cho đối tượng
+    public class GroupMeeting
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Enter project Name!")]
+        public string ProjectName { get; set; }
+        [Required(ErrorMessage = "Enter Lead Name!")]
+        public string GroupMeetingLeadName { get; set; }
+
+        [Required(ErrorMessage = "Enter Team Lead Name!")]
+        public string TeamLeadName { get; set; }
+
+        [Required(ErrorMessage = "Enter Descciption!")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Enter Group Meeting Date! ")]
+        public DateTime GroupMeetingDate { get; set; }
+
+        public static string strConnectionString = "Data Source=DESKTOP-APIVU2V/SQLEXPRESS;Initial Catalog=ProjectMeeting;Integrated Security=True";
+
+    }
+}
+
+
+
+
+    
+  
+ 
+
+
